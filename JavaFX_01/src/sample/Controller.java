a0f576358e09f5f527609e48a9b3deac2a5d68b9
shpak <serg.shp@gmail.com>
2016-06-23 16:14:26 +0300

    New JavaFX window 21.06

--- JavaFX_01/src/sample/Controller.java
+++ JavaFX_01/src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public void okButtonAction(ActionEvent actionEvent) {
+        System.out.println("OK");
+    }
+
+    public void cancelButtonAction(ActionEvent actionEvent) {
+
+
+    }
 }
